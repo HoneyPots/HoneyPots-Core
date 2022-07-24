0be791b3681fe53e7f2bb0df7c4fcf5850a01b97
Dokyeongyun <aservmz@naver.com>
2022-07-25 03:09:28 +0900

    Set initial gradle project and add springboot dependency

---
+++ src/main/java/com/honeypot/HoneyPotApplication.java
@@ -0,0 +1,12 @@
+package com.honeypot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneyPotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HoneyPotApplication.class, args);
+    }
+
+}
