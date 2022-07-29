0cfeca2b42a54385e9dd3563ab0eb75163e573f9
Dokyeongyun <aservmz@naver.com>
2022-07-29 12:53:54 +0900

    Add custom constraints and validator (@AllowedSortProperties) to validate allowed sort properties

--- src/main/java/com/honeypot/domain/board/api/NormalPostApi.java
+++ src/main/java/com/honeypot/domain/board/api/NormalPostApi.java
@@ -1,11 +1,13 @@
 package com.honeypot.domain.board.api;
 
+import com.honeypot.common.validation.constraints.AllowedSortProperties;
 import com.honeypot.domain.board.dto.NormalPostDto;
 import com.honeypot.domain.board.dto.NormalPostUploadRequest;
 import com.honeypot.domain.board.service.NormalPostUploadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -14,19 +16,19 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api/post/normal")
 @RequiredArgsConstructor
+@Validated
 public class NormalPostApi {
 
     private final NormalPostUploadService normalPostUploadService;
 
     @GetMapping
-    public ResponseEntity<?> pageList(Pageable pageable) {
+    public ResponseEntity<?> pageList(@AllowedSortProperties("createdAt") Pageable pageable) {
         return ResponseEntity.ok(normalPostUploadService.pageList(pageable));
     }
 
     @PostMapping
     public ResponseEntity<?> upload(@RequestHeader("Authorization") String token,
                                     @Valid @RequestBody NormalPostUploadRequest uploadRequest) {
-
 
         // TODO validate token and find member id
         long writerId = 1;
