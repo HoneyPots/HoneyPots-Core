a76e548ab21678c1ac6ab37e217ce4cb638fdbfe
Dokyeongyun <aservmz@naver.com>
2022-07-31 04:37:55 +0900

    Add dto ↔ entity conversion mapper between 'CommentReaction' and 'ReactionDto'

---
+++ src/main/java/com/honeypot/domain/board/mapper/CommentReactionMapper.java
@@ -0,0 +1,34 @@
+package com.honeypot.domain.board.mapper;
+
+import com.honeypot.domain.board.dto.ReactionDto;
+import com.honeypot.domain.board.dto.ReactionRequest;
+import com.honeypot.domain.board.entity.CommentReaction;
+import com.honeypot.domain.board.entity.PostReaction;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CommentReactionMapper {
+
+    @Mapping(target = "reactor.gender", ignore = true)
+    @Mapping(target = "reactor.birthday", ignore = true)
+    @Mapping(target = "reactor.ageRange", ignore = true)
+    @Mapping(target = "reactor.email", ignore = true)
+    @Mapping(target = "reactor.createdAt", ignore = true)
+    @Mapping(target = "reactor.lastModifiedAt", ignore = true)
+    @Mapping(source = "reactionId", target = "id")
+    @Mapping(source = "reactor.id", target = "reactor.id")
+    @Mapping(target = "comment.id", expression = "java(reactionDto.getTargetType().equals(\"COMMENT\") ? reactionDto.getTargetId() : null)")
+    CommentReaction toEntity(ReactionDto reactionDto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(source = "targetId", target = "comment.id")
+    @Mapping(source = "reactorId", target = "reactor.id")
+    CommentReaction toEntity(ReactionRequest dto);
+
+    @InheritInverseConfiguration
+    @Mapping(source = "comment.id", target = "targetId")
+    ReactionDto toDto(CommentReaction entity);
+
+}
