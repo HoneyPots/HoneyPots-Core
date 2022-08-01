148744bb5b212335d26bad4366153a9b91262728
Dokyeongyun <aservmz@naver.com>
2022-08-02 03:21:31 +0900

    Add properties related with jwt

--- src/main/java/com/honeypot/common/config/PropertiesConfig.java
+++ src/main/java/com/honeypot/common/config/PropertiesConfig.java
@@ -1,10 +1,11 @@
 package com.honeypot.common.config;
 
+import com.honeypot.common.model.properties.JwtProperties;
 import com.honeypot.common.model.properties.KakaoProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties(KakaoProperties.class)
+@EnableConfigurationProperties({KakaoProperties.class, JwtProperties.class})
 public class PropertiesConfig {
 }
