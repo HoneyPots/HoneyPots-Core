11fd45e7a022821ff671ea78e020b553428eaaab
Dokyeongyun <aservmz@naver.com>
2022-08-02 04:23:20 +0900

    Add Jwt library dependency and Implement stubbing class ('JwtAuthTokenManagerService')

--- src/main/java/com/honeypot/domain/auth/service/JwtAuthTokenManagerService.java
+++ src/main/java/com/honeypot/domain/auth/service/JwtAuthTokenManagerService.java
@@ -1,36 +1,72 @@
 package com.honeypot.domain.auth.service;
 
+import com.honeypot.common.model.properties.JwtProperties;
 import com.honeypot.domain.auth.service.contracts.AuthTokenManagerService;
-import com.honeypot.domain.member.entity.Member;
-import com.honeypot.domain.member.repository.MemberRepository;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.SecurityException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
-import javax.persistence.EntityNotFoundException;
 import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Validated
 public class JwtAuthTokenManagerService implements AuthTokenManagerService {
 
-    private final MemberRepository memberRepository;
+    private final JwtProperties jwtProperties;
 
+    @Override
     public String issue(@NotNull Long memberId) {
-        Member member = memberRepository
-                .findById(memberId)
-                .orElseThrow(EntityNotFoundException::new);
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime expireAt = now.plus(jwtProperties.expirationTimeInSeconds().accessToken(), ChronoUnit.SECONDS);
 
-        return member.getId() + "_" + member.getNickname();
+        return Jwts
+                .builder()
+                .signWith(jwtProperties.generateSecretKey(), SignatureAlgorithm.HS512)
+                .setSubject(String.valueOf(memberId))
+                .setIssuedAt(Timestamp.valueOf(now))
+                .setExpiration(Timestamp.valueOf(expireAt))
+                .compact();
     }
 
+    @Override
     public boolean validate(@NotNull String token) {
-        return token.split("_").length == 2;
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(jwtProperties.generateSecretKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException e) {
+            log.info("Invalid JWT signature.", e);
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT token.", e);
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT token.", e);
+        } catch (IllegalArgumentException e) {
+            log.info("Invalid JWT.", e);
+        }
+
+        return false;
     }
 
+    @Override
     public Long getMemberId(@NotNull String token) {
-        return Long.parseLong(token.split("_")[0]);
+        Claims claims = Jwts
+                .parserBuilder()
+                .setSigningKey(jwtProperties.generateSecretKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return Long.valueOf(claims.getSubject());
     }
 
 }
