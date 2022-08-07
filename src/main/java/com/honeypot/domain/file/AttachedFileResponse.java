90948ab39e6d6ee3ae725e32f74327a03419ae61
KyeongYun Do <aservmz@naver.com>
2022-08-08 04:25:27 +0900

    Feature/post attach file (#47)
    * Write query that inquiry post list with thumbnail image
    
    * Optimize query that inquiry single post detail and Add attached file field

--- src/main/java/com/honeypot/domain/file/AttachedFileResponse.java
+++ src/main/java/com/honeypot/domain/file/AttachedFileResponse.java
@@ -1,11 +1,18 @@
 package com.honeypot.domain.file;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class AttachedFileResponse {
+
+    @QueryProjection
+    public AttachedFileResponse(Long fileId, String fileLocationUrl) {
+        this.fileId = fileId;
+        this.fileLocationUrl = fileLocationUrl;
+    }
 
     private Long fileId;
 
