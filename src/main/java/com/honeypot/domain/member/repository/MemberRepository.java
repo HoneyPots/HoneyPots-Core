49daf2f2bc0aa3f77c90905746fa9f16f55e83aa
Dokyeongyun <aservmz@naver.com>
2022-08-10 03:11:02 +0900

    TDD: Double-check whether nickname is available

--- src/main/java/com/honeypot/domain/member/repository/MemberRepository.java
+++ src/main/java/com/honeypot/domain/member/repository/MemberRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    boolean existsByNickname(String nickname);
+
 }
