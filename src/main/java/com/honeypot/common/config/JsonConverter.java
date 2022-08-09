16d33f379d52644233e1cb4fcb33af2a449dc683
Dokyeongyun <aservmz@naver.com>
2022-08-10 01:35:07 +0900

    Add JsonConverter for deserialize 'LocalDateTime' type (from Unix timestamp to LocalDateTime)

---
+++ src/main/java/com/honeypot/common/config/JsonConverter.java
@@ -0,0 +1,24 @@
+package com.honeypot.common.config;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import org.springframework.boot.jackson.JsonComponent;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+@JsonComponent
+public class JsonConverter {
+
+    public static class LocalDateTimeJsonDeserializer extends JsonDeserializer<LocalDateTime> {
+        @Override
+        public LocalDateTime deserialize(JsonParser jsonParser,
+                                         DeserializationContext deserializationContext) throws IOException {
+            Date in = new Date(jsonParser.getValueAsLong()*1000);
+            return LocalDateTime.ofInstant(in.toInstant(), ZoneId.systemDefault());
+        }
+    }
+}
