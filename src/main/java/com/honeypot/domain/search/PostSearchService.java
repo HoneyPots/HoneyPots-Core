128dcce71a786db3af316325a4edec260370c671
Dokyeongyun <aservmz@naver.com>
2022-08-12 20:59:47 +0900

    Add api endpoint that searching post list

---
+++ src/main/java/com/honeypot/domain/search/PostSearchService.java
@@ -0,0 +1,33 @@
+package com.honeypot.domain.search;
+
+import com.honeypot.domain.post.dto.PostDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.Valid;
+
+@Service
+@RequiredArgsConstructor
+@Validated
+public class PostSearchService {
+
+    private final PostSearchQuerydslRepository postSearchQuerydslRepository;
+
+    @Transactional(readOnly = true)
+    public Page<? extends PostDto> search(@Valid PostSearchCriteria criteria,
+                                          Pageable pageable,
+                                          Long memberId) {
+        Page<? extends PostDto> result = postSearchQuerydslRepository.findAllPost(criteria, pageable, memberId);
+        return new PageImpl<>(
+                result.getContent(),
+                pageable,
+                result.getTotalElements()
+        );
+    }
+
+}
