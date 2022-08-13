463273f9d2b9511d3284e4654546a7fcbf117d02
Dokyeongyun <aservmz@naver.com>
2022-08-14 02:55:57 +0900

    Add api endpoint that get members profiles for double-check nickname value

--- src/main/java/com/honeypot/domain/member/api/MemberApi.java
+++ src/main/java/com/honeypot/domain/member/api/MemberApi.java
@@ -9,12 +9,15 @@ import com.honeypot.domain.member.dto.NicknameModifyRequest;
 import com.honeypot.domain.member.service.MemberNicknameModifyService;
 import com.honeypot.domain.member.service.MemberWithdrawService;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,6 +32,22 @@ public class MemberApi {
     private final MemberWithdrawService memberWithdrawService;
 
     private final ObjectMapper objectMapper;
+
+    @GetMapping(value = "/profile", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getNicknameProfile(@RequestParam @NotBlank @Length(max = 20) String nickname)
+            throws JsonProcessingException {
+
+        if (!memberNicknameModifyService.isAvailableNickname(nickname)) {
+            Map<String, Object> responseBody = new HashMap<>();
+            responseBody.put("nickname", nickname);
+
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(objectMapper.writeValueAsString(responseBody));
+        }
+
+        return ResponseEntity.notFound().build();
+    }
 
     @PatchMapping("/{memberId}/profile/nickname")
     public ResponseEntity<?> changeNickname(@PathVariable Long memberId,
