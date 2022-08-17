0cc0ac499e7f6a4001a1ac496f74d16388ddeb77
Dokyeongyun <aservmz@naver.com>
2022-08-17 16:12:40 +0900

    TDD: Improve test coverage for 'NormalPostMapperImpl' (57.8% → 97%)

--- src/main/java/com/honeypot/domain/file/PostFileUploadRequest.java
+++ src/main/java/com/honeypot/domain/file/PostFileUploadRequest.java
@@ -1,11 +1,13 @@
 package com.honeypot.domain.file;
 
 import com.honeypot.common.validation.groups.InsertContext;
+import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 
 @Data
+@Builder
 public class PostFileUploadRequest {
 
     @NotNull
