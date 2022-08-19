f6af85a440cfa2c10519ca703834b2bd6ef34a52
KyeongYun Do <aservmz@naver.com>
2022-08-19 10:26:46 +0900

    Feature/post domain test coverage (#93)
    * TDD: Improve test coverage for 'NormalPostMapperImpl' (57.8% → 97%)
    
    * TDD: Improve test coverage for 'UsedTradePostMapperImpl' (65.6% → 97%)
    
    * TDD: Improve test coverage for 'GroupBuyingPostMapperImpl' (65.6% → 97%)
    
    * TDD: Improve test coverage for 'NormalPostQuerydslRepository' (0% → 100%)
    
    * TDD: Improve test coverage for 'UsedTradePostQuerydslRepository' (0% → 100%)
    
    * TDD: Improve test coverage for 'GroupBuyingPostQuerydslRepository' (0% → 100%)

--- src/test/java/com/honeypot/domain/post/mapper/GroupBuyingPostMapperTest.java
+++ src/test/java/com/honeypot/domain/post/mapper/GroupBuyingPostMapperTest.java
@@ -1,6 +1,10 @@
 package com.honeypot.domain.post.mapper;
 
 import com.honeypot.domain.comment.entity.Comment;
+import com.honeypot.domain.file.AttachedFileResponse;
+import com.honeypot.domain.file.File;
+import com.honeypot.domain.file.FileType;
+import com.honeypot.domain.file.PostFileUploadRequest;
 import com.honeypot.domain.member.dto.WriterDto;
 import com.honeypot.domain.member.entity.Member;
 import com.honeypot.domain.post.dto.GroupBuyingPostDto;
@@ -12,66 +16,90 @@ import org.mapstruct.factory.Mappers;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class GroupBuyingPostMapperTest {
 
     private final GroupBuyingPostMapper mapper = Mappers.getMapper(GroupBuyingPostMapper.class);
 
     @Test
-    void toEntity() {
+    void toEntity_WhenDtoIsNull() {
         // Arrange
-        LocalDateTime uploadedAt = LocalDateTime.of(2022, 7, 28, 22, 0, 0);
-        GroupBuyingPostDto dto = GroupBuyingPostDto.builder()
-                .postId(1)
-                .title("title")
-                .content("content")
-                .writer(WriterDto.builder()
-                        .id(1)
-                        .nickname("nickname")
-                        .build())
-                .commentCount(10L)
-                .likeReactionCount(5L)
-                .isLiked(true)
-                .likeReactionId(10L)
-                .thumbnailImageFile(null)
-                .uploadedAt(uploadedAt)
-                .lastModifiedAt(uploadedAt)
-
-                .category("한식")
-                .groupBuyingStatus(GroupBuyingStatus.COMPLETE)
-                .chatRoomLink(null)
-                .deadline(LocalDateTime.now().plus(20000L, ChronoUnit.SECONDS))
-                .build();
+        GroupBuyingPostDto dto = null;
 
         // Act
         GroupBuyingPost entity = mapper.toEntity(dto);
 
         // Assert
-        assertEquals(dto.getPostId(), entity.getId());
-        assertEquals(dto.getTitle(), entity.getTitle());
-        assertEquals(dto.getContent(), entity.getContent());
-        assertEquals(dto.getWriter().getId(), entity.getWriter().getId());
-        assertEquals(dto.getWriter().getNickname(), entity.getWriter().getNickname());
-        assertEquals(dto.getCategory(), entity.getCategory());
-        assertEquals(dto.getGroupBuyingStatus(), entity.getGroupBuyingStatus());
-        assertEquals(dto.getChatRoomLink(), entity.getChatRoomLink());
-        assertEquals(dto.getDeadline(), entity.getDeadline());
-        assertEquals(dto.getUploadedAt(), entity.getCreatedAt());
-        assertEquals(dto.getLastModifiedAt(), entity.getLastModifiedAt());
+        assertNull(entity);
     }
 
     @Test
-    void toEntity_FromUsedTradePostUploadRequest() {
+    void toEntity() {
         // Arrange
+        WriterDto writerDto = WriterDto.builder()
+                .id(1)
+                .nickname("nickname")
+                .build();
+        List<AttachedFileResponse> attachedFileResponses = new ArrayList<>();
+        attachedFileResponses.add(AttachedFileResponse.builder()
+                .fileId(1L)
+                .fileLocationUrl("http://file-server.com/img/img.png")
+                .build());
+        attachedFileResponses.add(null);
+        GroupBuyingPostDto dto = createDtoWithAnyData(123L, writerDto, attachedFileResponses);
+
+        // Act
+        GroupBuyingPost entity = mapper.toEntity(dto);
+
+        // Assert
+        assertEntity(dto, entity);
+    }
+
+    @Test
+    void toEntity_WhenWriterDtoAndAttachFileListIsNull() {
+        // Arrange
+        WriterDto writerDto = null;
+        List<AttachedFileResponse> attachedFileResponses = null;
+        GroupBuyingPostDto dto = createDtoWithAnyData(11L, writerDto, attachedFileResponses);
+
+        // Act
+        GroupBuyingPost entity = mapper.toEntity(dto);
+
+        // Assert
+        assertEntity(dto, entity);
+    }
+
+    @Test
+    void toEntity_FromGroupBuyingPostUploadRequest_WhenDtoIsNull() {
+        // Arrange
+        GroupBuyingPostUploadRequest dto = null;
+
+        // Act
+        GroupBuyingPost entity = mapper.toEntity(dto);
+
+        // Assert
+        assertNull(entity);
+    }
+
+    @Test
+    void toEntity_FromGroupBuyingPostUploadRequest() {
+        // Arrange
+        List<PostFileUploadRequest> postFileUploadRequests = new ArrayList<>();
+        postFileUploadRequests.add(PostFileUploadRequest.builder()
+                .fileId(1L)
+                .willBeUploaded(true)
+                .linkPostId(12L)
+                .build());
+        postFileUploadRequests.add(null);
         GroupBuyingPostUploadRequest dto = GroupBuyingPostUploadRequest.builder()
                 .title("title")
                 .content("content")
                 .writerId(1L)
-                .attachedFiles(null)
+                .attachedFiles(postFileUploadRequests)
                 .category("한식")
                 .groupBuyingStatus("ONGOING")
                 .chatRoomLink("chatRoomLink")
@@ -90,18 +118,85 @@ class GroupBuyingPostMapperTest {
     }
 
     @Test
+    void toDto_WhenEntityIsNull() {
+        // Arrange
+        GroupBuyingPost entity = null;
+
+        // Act
+        GroupBuyingPostDto dto = mapper.toDto(entity);
+
+        // Assert
+        assertNull(dto);
+    }
+
+    @Test
     void toDto() {
         // Arrange
-        LocalDateTime uploadedAt = LocalDateTime.of(2022, 7, 28, 22, 0, 0);
-        GroupBuyingPost entity = GroupBuyingPost.builder()
+        Member writer = Member.builder()
                 .id(1L)
+                .nickname("nickname")
+                .build();
+        List<File> attachedFiles = new ArrayList<>();
+        attachedFiles.add(File.builder()
+                .id(1123L)
+                .fileType(FileType.NORMAL_POST_IMAGE)
+                .filePath("img/")
+                .filename("name,png")
+                .originalFilename("original.png")
+                .build());
+        attachedFiles.add(null);
+        GroupBuyingPost entity = createEntityWithAnyData(1L, writer, attachedFiles);
+
+        // Act
+        GroupBuyingPostDto dto = mapper.toDto(entity);
+
+        // Assert
+        assertDto(entity, dto);
+    }
+
+    @Test
+    void toDtoList_FromEntityList_WhenEntityListIsNull() {
+        // Arrange
+        List<GroupBuyingPost> entities = null;
+
+        // Act
+        List<GroupBuyingPostDto> dtoList = mapper.toDto(entities);
+
+        // Assert
+        assertNull(dtoList);
+    }
+
+    @Test
+    void toDtoList_FromEntityList() {
+        // Arrange
+        int listSize = 10;
+        List<GroupBuyingPost> entities = new ArrayList<>();
+        for (long i = 0; i < listSize; i++) {
+            entities.add(createEntityWithAnyData(i, null, null));
+        }
+
+        // Act
+        List<GroupBuyingPostDto> dtoList = mapper.toDto(entities);
+
+        // Assert
+        assertNotNull(dtoList);
+        assertEquals(listSize, dtoList.size());
+        for (int i = 0; i < listSize; i++) {
+            GroupBuyingPost entity = entities.get(i);
+            GroupBuyingPostDto dto = dtoList.get(i);
+            assertDto(entity, dto);
+        }
+    }
+
+    private GroupBuyingPost createEntityWithAnyData(Long id, Member writer, List<File> attachedFiles) {
+        LocalDateTime uploadedAt = LocalDateTime.now();
+        return GroupBuyingPost.builder()
+                .id(id)
                 .title("title")
                 .content("content")
-                .writer(Member.builder()
-                        .id(1L)
-                        .nickname("nickname")
-                        .build())
+                .writer(writer)
                 .comments(List.of(Comment.builder().build()))
+                .attachedFiles(attachedFiles)
                 .createdAt(uploadedAt)
                 .lastModifiedAt(uploadedAt)
 
@@ -110,16 +205,18 @@ class GroupBuyingPostMapperTest {
                 .chatRoomLink("https://example.com/adfsafsdca")
                 .deadline(LocalDateTime.now())
                 .build();
+    }
 
-        // Act
-        GroupBuyingPostDto dto = mapper.toDto(entity);
-
-        // Assert
+    private void assertDto(GroupBuyingPost entity, GroupBuyingPostDto dto) {
         assertEquals(entity.getId(), dto.getPostId());
         assertEquals(entity.getTitle(), dto.getTitle());
         assertEquals(entity.getContent(), dto.getContent());
-        assertEquals(entity.getWriter().getId(), dto.getWriter().getId());
-        assertEquals(entity.getWriter().getNickname(), dto.getWriter().getNickname());
+        if (entity.getWriter() != null) {
+            assertEquals(entity.getWriter().getId(), dto.getWriter().getId());
+            assertEquals(entity.getWriter().getNickname(), dto.getWriter().getNickname());
+        } else {
+            assertNull(dto.getWriter());
+        }
         assertNull(dto.getCommentCount());
         assertNull(dto.getLikeReactionCount());
         assertNull(dto.getLikeReactionId());
@@ -130,6 +227,53 @@ class GroupBuyingPostMapperTest {
         assertEquals(entity.getGroupBuyingStatus(), dto.getGroupBuyingStatus());
         assertEquals(entity.getChatRoomLink(), dto.getChatRoomLink());
         assertEquals(entity.getDeadline(), dto.getDeadline());
+    }
+
+    private GroupBuyingPostDto createDtoWithAnyData(Long id, WriterDto writer,
+                                                    List<AttachedFileResponse> attachedFileResponses) {
+        LocalDateTime uploadedAt = LocalDateTime.now();
+        AttachedFileResponse thumbnail = null;
+        if (attachedFileResponses != null && !attachedFileResponses.isEmpty()) {
+            thumbnail = attachedFileResponses.get(0);
+        }
+
+        return GroupBuyingPostDto.builder()
+                .postId(id)
+                .title("title")
+                .content("content")
+                .writer(writer)
+                .commentCount(10L)
+                .likeReactionCount(5L)
+                .isLiked(true)
+                .likeReactionId(10L)
+                .thumbnailImageFile(thumbnail)
+                .attachedFiles(attachedFileResponses)
+                .uploadedAt(uploadedAt)
+                .lastModifiedAt(uploadedAt)
+
+                .category("한식")
+                .groupBuyingStatus(GroupBuyingStatus.COMPLETE)
+                .chatRoomLink(null)
+                .deadline(LocalDateTime.now().plus(20000L, ChronoUnit.SECONDS))
+                .build();
+    }
+
+    private void assertEntity(GroupBuyingPostDto dto, GroupBuyingPost entity) {
+        assertEquals(dto.getPostId(), entity.getId());
+        assertEquals(dto.getTitle(), entity.getTitle());
+        assertEquals(dto.getContent(), entity.getContent());
+        if (dto.getWriter() != null) {
+            assertEquals(dto.getWriter().getId(), entity.getWriter().getId());
+            assertEquals(dto.getWriter().getNickname(), entity.getWriter().getNickname());
+        } else {
+            assertNull(entity.getWriter());
+        }
+        assertEquals(dto.getCategory(), entity.getCategory());
+        assertEquals(dto.getGroupBuyingStatus(), entity.getGroupBuyingStatus());
+        assertEquals(dto.getChatRoomLink(), entity.getChatRoomLink());
+        assertEquals(dto.getDeadline(), entity.getDeadline());
+        assertEquals(dto.getUploadedAt(), entity.getCreatedAt());
+        assertEquals(dto.getLastModifiedAt(), entity.getLastModifiedAt());
     }
 
 }
--- src/test/java/com/honeypot/domain/post/mapper/UsedTradePostMapperTest.java
+++ src/test/java/com/honeypot/domain/post/mapper/UsedTradePostMapperTest.java
@@ -1,6 +1,10 @@
 package com.honeypot.domain.post.mapper;
 
 import com.honeypot.domain.comment.entity.Comment;
+import com.honeypot.domain.file.AttachedFileResponse;
+import com.honeypot.domain.file.File;
+import com.honeypot.domain.file.FileType;
+import com.honeypot.domain.file.PostFileUploadRequest;
 import com.honeypot.domain.member.dto.WriterDto;
 import com.honeypot.domain.member.entity.Member;
 import com.honeypot.domain.post.dto.UsedTradePostDto;
@@ -12,66 +16,90 @@ import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class UsedTradePostMapperTest {
 
     private final UsedTradePostMapper mapper = Mappers.getMapper(UsedTradePostMapper.class);
 
     @Test
-    void toEntity() {
+    void toEntity_WhenDtoIsNull() {
         // Arrange
-        LocalDateTime uploadedAt = LocalDateTime.of(2022, 7, 28, 22, 0, 0);
-        UsedTradePostDto dto = UsedTradePostDto.builder()
-                .postId(1)
-                .title("title")
-                .content("content")
-                .writer(WriterDto.builder()
-                        .id(1)
-                        .nickname("nickname")
-                        .build())
-                .commentCount(10L)
-                .likeReactionCount(5L)
-                .isLiked(true)
-                .likeReactionId(10L)
-                .thumbnailImageFile(null)
-                .uploadedAt(uploadedAt)
-                .lastModifiedAt(uploadedAt)
-
-                .goodsPrice(10000)
-                .tradeType(TradeType.SELL)
-                .tradeStatus(TradeStatus.ONGOING)
-                .chatRoomLink(null)
-                .build();
+        UsedTradePostDto dto = null;
 
         // Act
         UsedTradePost entity = mapper.toEntity(dto);
 
         // Assert
-        assertEquals(dto.getPostId(), entity.getId());
-        assertEquals(dto.getTitle(), entity.getTitle());
-        assertEquals(dto.getContent(), entity.getContent());
-        assertEquals(dto.getWriter().getId(), entity.getWriter().getId());
-        assertEquals(dto.getWriter().getNickname(), entity.getWriter().getNickname());
-        assertEquals(dto.getGoodsPrice(), entity.getGoodsPrice());
-        assertEquals(dto.getTradeType(), entity.getTradeType());
-        assertEquals(dto.getTradeStatus(), entity.getTradeStatus());
-        assertEquals(dto.getChatRoomLink(), entity.getChatRoomLink());
-        assertEquals(dto.getUploadedAt(), entity.getCreatedAt());
-        assertEquals(dto.getLastModifiedAt(), entity.getLastModifiedAt());
+        assertNull(entity);
+    }
+
+    @Test
+    void toEntity() {
+        // Arrange
+        WriterDto writerDto = WriterDto.builder()
+                .id(1)
+                .nickname("nickname")
+                .build();
+        List<AttachedFileResponse> attachedFileResponses = new ArrayList<>();
+        attachedFileResponses.add(AttachedFileResponse.builder()
+                .fileId(1L)
+                .fileLocationUrl("http://file-server.com/img/img.png")
+                .build());
+        attachedFileResponses.add(null);
+        UsedTradePostDto dto = createDtoWithAnyData(11L, writerDto, attachedFileResponses);
+
+        // Act
+        UsedTradePost entity = mapper.toEntity(dto);
+
+        // Assert
+        assertEntity(dto, entity);
+    }
+
+    @Test
+    void toEntity_WhenWriterDtoAndAttachFileListIsNull() {
+        // Arrange
+        WriterDto writerDto = null;
+        List<AttachedFileResponse> attachedFileResponses = null;
+        UsedTradePostDto dto = createDtoWithAnyData(11L, writerDto, attachedFileResponses);
+
+        // Act
+        UsedTradePost entity = mapper.toEntity(dto);
+
+        // Assert
+        assertEntity(dto, entity);
+    }
+
+    @Test
+    void toEntity_FromUsedTradePostUploadRequest_WhenDtoIsNull() {
+        // Arrange
+        UsedTradePostUploadRequest dto = null;
+
+        // Act
+        UsedTradePost entity = mapper.toEntity(dto);
+
+        // Assert
+        assertNull(entity);
     }
 
     @Test
     void toEntity_FromUsedTradePostUploadRequest() {
         // Arrange
+        List<PostFileUploadRequest> postFileUploadRequests = new ArrayList<>();
+        postFileUploadRequests.add(PostFileUploadRequest.builder()
+                .fileId(1L)
+                .willBeUploaded(true)
+                .linkPostId(12L)
+                .build());
+        postFileUploadRequests.add(null);
         UsedTradePostUploadRequest dto = UsedTradePostUploadRequest.builder()
                 .title("title")
                 .content("content")
                 .writerId(1L)
-                .attachedFiles(null)
+                .attachedFiles(postFileUploadRequests)
                 .goodsPrice(20000)
                 .tradeStatus(TradeStatus.COMPLETE.toString())
                 .tradeType(TradeType.BUY.toString())
@@ -91,18 +119,85 @@ class UsedTradePostMapperTest {
     }
 
     @Test
+    void toDto_WhenEntityIsNull() {
+        // Arrange
+        UsedTradePost entity = null;
+
+        // Act
+        UsedTradePostDto dto = mapper.toDto(entity);
+
+        // Assert
+        assertNull(dto);
+    }
+
+    @Test
     void toDto() {
         // Arrange
-        LocalDateTime uploadedAt = LocalDateTime.of(2022, 7, 28, 22, 0, 0);
-        UsedTradePost entity = UsedTradePost.builder()
+        Member writer = Member.builder()
                 .id(1L)
+                .nickname("nickname")
+                .build();
+        List<File> attachedFiles = new ArrayList<>();
+        attachedFiles.add(File.builder()
+                .id(1123L)
+                .fileType(FileType.NORMAL_POST_IMAGE)
+                .filePath("img/")
+                .filename("name,png")
+                .originalFilename("original.png")
+                .build());
+        attachedFiles.add(null);
+        UsedTradePost entity = createEntityWithAnyData(1L, writer, attachedFiles);
+
+        // Act
+        UsedTradePostDto dto = mapper.toDto(entity);
+
+        // Assert
+        assertDto(entity, dto);
+    }
+
+    @Test
+    void toDtoList_FromEntityList_WhenEntityListIsNull() {
+        // Arrange
+        List<UsedTradePost> entities = null;
+
+        // Act
+        List<UsedTradePostDto> dtoList = mapper.toDto(entities);
+
+        // Assert
+        assertNull(dtoList);
+    }
+
+    @Test
+    void toDtoList_FromEntityList() {
+        // Arrange
+        int listSize = 10;
+        List<UsedTradePost> entities = new ArrayList<>();
+        for (long i = 0; i < listSize; i++) {
+            entities.add(createEntityWithAnyData(i, null, null));
+        }
+
+        // Act
+        List<UsedTradePostDto> dtoList = mapper.toDto(entities);
+
+        // Assert
+        assertNotNull(dtoList);
+        assertEquals(listSize, dtoList.size());
+        for (int i = 0; i < listSize; i++) {
+            UsedTradePost entity = entities.get(i);
+            UsedTradePostDto dto = dtoList.get(i);
+            assertDto(entity, dto);
+        }
+    }
+
+    private UsedTradePost createEntityWithAnyData(Long id, Member writer, List<File> attachedFiles) {
+        LocalDateTime uploadedAt = LocalDateTime.now();
+        return UsedTradePost.builder()
+                .id(id)
                 .title("title")
                 .content("content")
-                .writer(Member.builder()
-                        .id(1L)
-                        .nickname("nickname")
-                        .build())
+                .writer(writer)
                 .comments(List.of(Comment.builder().build()))
+                .attachedFiles(attachedFiles)
                 .createdAt(uploadedAt)
                 .lastModifiedAt(uploadedAt)
 
@@ -111,16 +206,18 @@ class UsedTradePostMapperTest {
                 .tradeStatus(TradeStatus.COMPLETE)
                 .chatRoomLink("https://example.com/adfsafsdca")
                 .build();
+    }
 
-        // Act
-        UsedTradePostDto dto = mapper.toDto(entity);
-
-        // Assert
+    private void assertDto(UsedTradePost entity, UsedTradePostDto dto) {
         assertEquals(entity.getId(), dto.getPostId());
         assertEquals(entity.getTitle(), dto.getTitle());
         assertEquals(entity.getContent(), dto.getContent());
-        assertEquals(entity.getWriter().getId(), dto.getWriter().getId());
-        assertEquals(entity.getWriter().getNickname(), dto.getWriter().getNickname());
+        if (entity.getWriter() != null) {
+            assertEquals(entity.getWriter().getId(), dto.getWriter().getId());
+            assertEquals(entity.getWriter().getNickname(), dto.getWriter().getNickname());
+        } else {
+            assertNull(dto.getWriter());
+        }
         assertNull(dto.getCommentCount());
         assertNull(dto.getLikeReactionCount());
         assertNull(dto.getLikeReactionId());
@@ -131,6 +228,52 @@ class UsedTradePostMapperTest {
         assertEquals(entity.getTradeStatus(), dto.getTradeStatus());
         assertEquals(entity.getTradeType(), dto.getTradeType());
         assertEquals(entity.getChatRoomLink(), dto.getChatRoomLink());
+    }
+
+    private UsedTradePostDto createDtoWithAnyData(Long id, WriterDto writer,
+                                                  List<AttachedFileResponse> attachedFileResponses) {
+        LocalDateTime uploadedAt = LocalDateTime.now();
+        AttachedFileResponse thumbnail = null;
+        if (attachedFileResponses != null && !attachedFileResponses.isEmpty()) {
+            thumbnail = attachedFileResponses.get(0);
+        }
+
+        return UsedTradePostDto.builder()
+                .postId(id)
+                .title("title")
+                .content("content")
+                .writer(writer)
+                .commentCount(10L)
+                .likeReactionCount(5L)
+                .isLiked(true)
+                .likeReactionId(10L)
+                .thumbnailImageFile(thumbnail)
+                .attachedFiles(attachedFileResponses)
+                .uploadedAt(uploadedAt)
+                .lastModifiedAt(uploadedAt)
+                .goodsPrice(10000)
+                .tradeType(TradeType.SELL)
+                .tradeStatus(TradeStatus.ONGOING)
+                .chatRoomLink(null)
+                .build();
+    }
+
+    private void assertEntity(UsedTradePostDto dto, UsedTradePost entity) {
+        assertEquals(dto.getPostId(), entity.getId());
+        assertEquals(dto.getTitle(), entity.getTitle());
+        assertEquals(dto.getContent(), entity.getContent());
+        if (dto.getWriter() != null) {
+            assertEquals(dto.getWriter().getId(), entity.getWriter().getId());
+            assertEquals(dto.getWriter().getNickname(), entity.getWriter().getNickname());
+        } else {
+            assertNull(entity.getWriter());
+        }
+        assertEquals(dto.getGoodsPrice(), entity.getGoodsPrice());
+        assertEquals(dto.getTradeType(), entity.getTradeType());
+        assertEquals(dto.getTradeStatus(), entity.getTradeStatus());
+        assertEquals(dto.getChatRoomLink(), entity.getChatRoomLink());
+        assertEquals(dto.getUploadedAt(), entity.getCreatedAt());
+        assertEquals(dto.getLastModifiedAt(), entity.getLastModifiedAt());
     }
 
 }
