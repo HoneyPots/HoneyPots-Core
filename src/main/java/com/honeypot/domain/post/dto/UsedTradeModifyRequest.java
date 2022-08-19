93d9dccb49c7362692934588d7343449b7a4ac57
Dokyeongyun <aservmz@naver.com>
2022-08-19 12:52:25 +0900

    TDD: Improve test coverage for 'UsedTradePostService' (3.4% → 100%)

--- src/main/java/com/honeypot/domain/post/dto/UsedTradeModifyRequest.java
+++ src/main/java/com/honeypot/domain/post/dto/UsedTradeModifyRequest.java
@@ -3,6 +3,7 @@ package com.honeypot.domain.post.dto;
 import com.honeypot.common.validation.constraints.Enum;
 import com.honeypot.common.validation.groups.InsertContext;
 import com.honeypot.domain.post.entity.enums.TradeStatus;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -12,6 +13,7 @@ import javax.validation.constraints.NotNull;
 @Getter
 @Setter
 @ToString
+@Builder
 public class UsedTradeModifyRequest {
 
     @Enum(target = TradeStatus.class)
