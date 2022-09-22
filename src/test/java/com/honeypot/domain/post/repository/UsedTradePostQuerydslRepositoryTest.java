fb3bc1ed1ed37a526bf6910897a713203b379e19
Dokyeongyun <aservmz@naver.com>
2022-09-23 03:13:38 +0900

    Add @TestConfig for Repository layer test to inject JPAQueryFactory dependency

--- src/test/java/com/honeypot/domain/post/repository/UsedTradePostQuerydslRepositoryTest.java
+++ src/test/java/com/honeypot/domain/post/repository/UsedTradePostQuerydslRepositoryTest.java
@@ -20,7 +20,6 @@ import com.honeypot.domain.reaction.repository.PostReactionRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
@@ -29,18 +28,17 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ActiveProfiles({"test", "aws"})
 @DataJpaTest
-@ExtendWith(SpringExtension.class)
 @Import(TestConfig.class)
 class UsedTradePostQuerydslRepositoryTest {
 
@@ -135,7 +133,7 @@ class UsedTradePostQuerydslRepositoryTest {
         Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Order.desc("createdAt")));
 
         Member writer = addMember();
-        Member anotherMember = addMember();
+        Member anotherMember = addMember(1L);
         for (int i = 0; i < postCount; i++) {
             if (i < myPostCount) {
                 addUsedTradePost(writer, null);
@@ -211,8 +209,12 @@ class UsedTradePostQuerydslRepositoryTest {
     }
 
     private Member addMember() {
+        return addMember(0L);
+    }
+
+    private Member addMember(Long id) {
         Member member = Member.builder()
-                .nickname("Member" + Timestamp.valueOf(LocalDateTime.now()).getTime())
+                .nickname("M" + Timestamp.valueOf(LocalDateTime.now()).getTime() + id)
                 .build();
         return memberRepository.save(member);
     }
