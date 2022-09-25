23e327af99ee3ef3eef2d6414fbe9dec2226ee0b
Dokyeongyun <aservmz@naver.com>
2022-09-25 17:33:58 +0900

    TDD: Save notification message asynchronously when notification event triggered

---
+++ src/main/java/com/honeypot/domain/notification/repository/NotificationRepository.java
@@ -0,0 +1,10 @@
+package com.honeypot.domain.notification.repository;
+
+import com.honeypot.domain.notification.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+}
--- src/test/java/com/honeypot/domain/notification/service/NotificationSendServiceTest.java
+++ src/test/java/com/honeypot/domain/notification/service/NotificationSendServiceTest.java
@@ -1,7 +1,11 @@
 package com.honeypot.domain.notification.service;
 
+import com.honeypot.domain.member.entity.Member;
+import com.honeypot.domain.member.service.MemberFindService;
 import com.honeypot.domain.notification.dto.NotificationTokenDto;
+import com.honeypot.domain.notification.entity.Notification;
 import com.honeypot.domain.notification.entity.enums.NotificationType;
+import com.honeypot.domain.notification.repository.NotificationRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,15 +15,22 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest(classes = NotificationSendService.class)
 @ActiveProfiles(profiles = {"test", "fcm"})
 class NotificationSendServiceTest {
 
     @MockBean
+    private MemberFindService memberFindService;
+
+    @MockBean
     private NotificationTokenManageService notificationTokenManageService;
+
+    @MockBean
+    private NotificationRepository notificationRepository;
 
     @Autowired
     private NotificationSendService notificationSendService;
@@ -56,6 +67,7 @@ class NotificationSendServiceTest {
     void send_toSingleMember() {
         // Arrange
         Long memberId = 1L;
+        NotificationType type = NotificationType.COMMENT_TO_MY_POST;
 
         String token = "f2JVeYJgpSuPx_2J4854lE:APA91bGL7lNvfZMR7_TQNXgbldxoyB11FuSoODvRggXywx4OUU7Zrg-b_1q3v5UDXwTtBi02CgHc6b9ZzF93FTHNXNpn8ewdxdhy5h8iG2gLy20y5mgj-x0yEfwx8iJ-zBDfcPQFMBae";
         String token2 = "eZPPh7NxWIiWMWSSRN1YLx:APA91bEnewBtElUaFzvlg_sz5f3B58o0KdGsVf3ONJH9ZnKjOhER59o__LPrGTy0qc99PuuwamX7EbT6yV7eAdiSLjiP-7YhF2XDrvYBFRBHD27V38Iqg81AGkh0d9t41Uy6QqsgJHma";
@@ -63,9 +75,35 @@ class NotificationSendServiceTest {
         tokens.add(NotificationTokenDto.builder().deviceToken(token).build());
         tokens.add(NotificationTokenDto.builder().deviceToken(token2).build());
 
+        Member member = Member.builder()
+                .id(memberId)
+                .nickname("nickname")
+                .build();
+
+        Notification notification = Notification.builder()
+                .member(member)
+                .message(type.getBody())
+                .type(type)
+                .build();
+
+        when(memberFindService.findById(memberId)).thenReturn(Optional.of(member));
         when(notificationTokenManageService.findByMemberId(memberId)).thenReturn(tokens);
+        when(notificationRepository.save(notification)).thenReturn(any(Notification.class));
+
+        // Act
+        notificationSendService.send(memberId, type);
+    }
+
+    @Test
+    void send_toSingleMember_WhenMemberNotFound() {
+        // Arrange
+        Long memberId = 1L;
+        when(memberFindService.findById(memberId)).thenReturn(Optional.empty());
 
         // Act
         notificationSendService.send(memberId, NotificationType.COMMENT_TO_MY_POST);
+
+        // Assert
+        verify(notificationRepository, never()).save(any(Notification.class));
     }
 }
