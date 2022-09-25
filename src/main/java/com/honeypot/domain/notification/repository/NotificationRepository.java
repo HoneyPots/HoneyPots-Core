021c347893623e51c02540eae1f3b7219c401958
Dokyeongyun <aservmz@naver.com>
2022-09-25 19:32:06 +0900

    TDD: Add service component that handles notification history

--- src/main/java/com/honeypot/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/honeypot/domain/notification/repository/NotificationRepository.java
@@ -1,10 +1,15 @@
 package com.honeypot.domain.notification.repository;
 
+import com.honeypot.domain.member.entity.Member;
 import com.honeypot.domain.notification.entity.Notification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    Page<Notification> findByMemberWithPagination(Member member, Pageable pageable);
 
 }
