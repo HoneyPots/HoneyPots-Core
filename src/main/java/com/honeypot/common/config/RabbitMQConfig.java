a08c85b037d62dd74f2146ba3c49fa0a1695ce88
Dokyeongyun <aservmz@naver.com>
2022-11-18 16:30:27 +0900

    Add configuration related with RabbitMQ (TopicExchange, Queue...)

---
+++ src/main/java/com/honeypot/common/config/RabbitMQConfig.java
@@ -0,0 +1,53 @@
+package com.honeypot.common.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    private final String exchangeName;
+
+    private final String queueName;
+    private final String routingKey;
+
+    public RabbitMQConfig(@Value("notification.exchange-name") String exchangeName,
+                          @Value("notification.queue-name") String queueName,
+                          @Value("notification.routing-key") String routingKey) {
+        this.exchangeName = exchangeName;
+        this.queueName = queueName;
+        this.routingKey = routingKey;
+    }
+
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(exchangeName);
+    }
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
+
+    @Bean
+    RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+}
