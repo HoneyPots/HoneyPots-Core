7e6cc94a85736ed6c36cee96187464fa7f1a3508
Dokyeongyun <aservmz@naver.com>
2022-11-20 23:23:01 +0900

    Send Comment & Reaction created event to MQ when listening that events

--- src/main/java/com/honeypot/common/event/CommentCreatedEventListener.java
+++ src/main/java/com/honeypot/common/event/CommentCreatedEventListener.java
@@ -1,12 +1,6 @@
 package com.honeypot.common.event;
 
-import com.honeypot.domain.comment.dto.CommentDto;
-import com.honeypot.domain.notification.dto.CommentNotificationResource;
-import com.honeypot.domain.notification.dto.NotificationData;
-import com.honeypot.domain.notification.dto.PostNotificationResource;
-import com.honeypot.domain.notification.entity.enums.NotificationType;
 import com.honeypot.domain.notification.service.NotificationSendService;
-import com.honeypot.domain.post.dto.SimplePostDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
@@ -16,45 +10,12 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class CommentCreatedEventListener {
 
-    private static final String MESSAGE_COMMENT_TO_POST = "'%s'님이 새로운 댓글을 남겼습니다.";
-
     private final NotificationSendService notificationSendService;
 
     @Async
     @EventListener
     public void listenCommentCreatedEvent(CommentCreatedEvent event) {
-        SimplePostDto targetPost = event.getTargetPost();
-        CommentDto createdComment = event.getCreatedComment();
-        if (isSameWriter(targetPost, createdComment)) {
-            return;
-        }
-
-        CommentNotificationResource resource = CommentNotificationResource.builder()
-                .postResource(PostNotificationResource.builder()
-                        .id(targetPost.getPostId())
-                        .type(targetPost.getPostType())
-                        .writer(targetPost.getWriter().getNickname())
-                        .build())
-                .commentId(createdComment.getCommentId())
-                .commenter(createdComment.getWriter().getNickname())
-                .build();
-
-        String commentContent = createdComment.getContent();
-        String contentMessage = commentContent.substring(0, Math.min(commentContent.length(), 100));
-        notificationSendService.send(
-                targetPost.getWriter().getId(),
-                NotificationData.<CommentNotificationResource>builder()
-                        .type(NotificationType.COMMENT_TO_POST)
-                        .titleMessage(String.format(MESSAGE_COMMENT_TO_POST, createdComment.getWriter().getNickname()))
-                        .contentMessage(contentMessage)
-                        .resource(resource)
-                        .build()
-        );
-
-    }
-
-    private boolean isSameWriter(SimplePostDto post, CommentDto comment) {
-        return comment.getWriter().getId().equals(post.getWriter().getId());
+        notificationSendService.send(event);
     }
 
 }
--- src/main/java/com/honeypot/common/event/ReactionCreatedEventListener.java
+++ src/main/java/com/honeypot/common/event/ReactionCreatedEventListener.java
@@ -1,13 +1,6 @@
 package com.honeypot.common.event;
 
-import com.honeypot.domain.notification.dto.NotificationData;
-import com.honeypot.domain.notification.dto.PostNotificationResource;
-import com.honeypot.domain.notification.dto.ReactionNotificationResource;
-import com.honeypot.domain.notification.entity.enums.NotificationType;
 import com.honeypot.domain.notification.service.NotificationSendService;
-import com.honeypot.domain.post.dto.SimplePostDto;
-import com.honeypot.domain.post.entity.Post;
-import com.honeypot.domain.reaction.dto.ReactionDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
@@ -17,46 +10,12 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ReactionCreatedEventListener {
 
-    private static final String MESSAGE_LIKE_REACTION_TO_POST = "'%s'님이 게시글을 좋아합니다.";
-
     private final NotificationSendService notificationSendService;
 
     @Async
     @EventListener
     public void listenReactionCreatedEvent(ReactionCreatedEvent event) {
-        SimplePostDto targetPost = event.getTargetPost();
-        ReactionDto createdReaction = event.getCreatedReaction();
-        if (isSameReactor(targetPost, createdReaction) || createdReaction.isAlreadyExists()) {
-            return;
-        }
-
-        ReactionNotificationResource resource = ReactionNotificationResource.builder()
-                .postResource(PostNotificationResource.builder()
-                        .id(targetPost.getPostId())
-                        .type(targetPost.getPostType())
-                        .writer(targetPost.getWriter().getNickname())
-                        .build())
-                .reactionId(createdReaction.getReactionId())
-                .reactionType(createdReaction.getReactionType())
-                .reactor(createdReaction.getReactor().getNickname())
-                .build();
-
-        String postTitle = targetPost.getTitle();
-        String contentMessage = postTitle.substring(0, Math.min(postTitle.length(), 100));
-        notificationSendService.send(
-                targetPost.getWriter().getId(),
-                NotificationData.<ReactionNotificationResource>builder()
-                        .type(NotificationType.LIKE_REACTION_TO_POST)
-                        .titleMessage(String.format(MESSAGE_LIKE_REACTION_TO_POST, createdReaction.getReactor().getNickname()))
-                        .contentMessage(contentMessage)
-                        .resource(resource)
-                        .build()
-        );
-    }
-
-
-    private boolean isSameReactor(SimplePostDto post, ReactionDto reaction) {
-        return reaction.getReactor().getId().equals(post.getWriter().getId());
+        notificationSendService.send(event);
     }
 
 }
