d33bb437b90242f9862d67541b4a09352ae683aa
Dokyeongyun <aservmz@naver.com>
2022-11-24 04:23:52 +0900

    Remove classes and interfaces related with notification token

--- src/main/java/com/honeypot/domain/notification/api/NotificationApi.java
+++ src/main/java/com/honeypot/domain/notification/api/NotificationApi.java
@@ -1,25 +1,18 @@
 package com.honeypot.domain.notification.api;
 
-import com.honeypot.common.model.exceptions.InvalidTokenException;
-import com.honeypot.common.utils.SecurityUtils;
 import com.honeypot.domain.notification.dto.NotificationResource;
-import com.honeypot.domain.notification.dto.NotificationTokenDto;
 import com.honeypot.domain.notification.service.NotificationHistoryService;
-import com.honeypot.domain.notification.service.NotificationTokenManageService;
-import com.honeypot.domain.notification.dto.NotificationTokenUploadRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/notifications")
 @RequiredArgsConstructor
 public class NotificationApi {
-
-    private final NotificationTokenManageService notificationTokenManageService;
 
     private final NotificationHistoryService notificationHistoryService;
 
@@ -30,30 +23,6 @@ public class NotificationApi {
         return ResponseEntity.ok(
                 notificationHistoryService.findNotificationResourceById(notificationId)
         );
-    }
-
-    @PostMapping("/tokens")
-    public ResponseEntity<?> saveNotificationToken(@Valid @RequestBody NotificationTokenUploadRequest request) {
-        Long memberId = SecurityUtils.getCurrentMemberId().orElseThrow(InvalidTokenException::new);
-        request.setMemberId(memberId);
-
-        NotificationTokenDto created = notificationTokenManageService.save(request);
-
-        return ResponseEntity
-                .created(ServletUriComponentsBuilder
-                        .fromCurrentRequest()
-                        .path("/{tokenId}")
-                        .buildAndExpand(created.getNotificationTokenId())
-                        .toUri())
-                .body(created);
-    }
-
-    @DeleteMapping("/tokens/{tokenId}")
-    public ResponseEntity<?> removeNotificationToken(@PathVariable Long tokenId) {
-        Long memberId = SecurityUtils.getCurrentMemberId().orElseThrow(InvalidTokenException::new);
-        notificationTokenManageService.remove(memberId, tokenId);
-
-        return ResponseEntity.noContent().build();
     }
 
 }
--- src/test/java/com/honeypot/domain/notification/api/NotificationApiTest.java
+++ src/test/java/com/honeypot/domain/notification/api/NotificationApiTest.java
@@ -3,10 +3,10 @@ package com.honeypot.domain.notification.api;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.honeypot.common.utils.SecurityUtils;
 import com.honeypot.domain.auth.service.contracts.AuthTokenManagerService;
-import com.honeypot.domain.notification.dto.*;
-import com.honeypot.domain.notification.entity.enums.ClientType;
+import com.honeypot.domain.notification.dto.CommentNotificationResource;
+import com.honeypot.domain.notification.dto.PostNotificationResource;
+import com.honeypot.domain.notification.dto.ReactionNotificationResource;
 import com.honeypot.domain.notification.service.NotificationHistoryServiceImpl;
-import com.honeypot.domain.notification.service.NotificationTokenManageServiceImpl;
 import com.honeypot.domain.post.entity.enums.PostType;
 import com.honeypot.domain.reaction.entity.enums.ReactionType;
 import org.apache.http.HttpHeaders;
@@ -25,21 +25,18 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import java.util.Optional;
-
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ActiveProfiles("test")
 @WebMvcTest(NotificationApi.class)
 @ExtendWith(MockitoExtension.class)
 @MockBean(classes = {JpaMetamodelMappingContext.class, AuthTokenManagerService.class})
 class NotificationApiTest {
-
-    @MockBean
-    private NotificationTokenManageServiceImpl notificationTokenManageService;
 
     @MockBean
     private NotificationHistoryServiceImpl notificationHistoryService;
@@ -131,104 +128,6 @@ class NotificationApiTest {
                 .andExpect(jsonPath("reactionId").value(resource.getReactionId()))
                 .andExpect(jsonPath("reactionType").value(resource.getReactionType().name()))
                 .andExpect(jsonPath("reactor").value(resource.getReactor()))
-                .andDo(print());
-    }
-
-    @Test
-    void saveNotificationToken_201_Created() throws Exception {
-        // Arrange
-        Long memberId = 1L;
-        NotificationTokenUploadRequest request = NotificationTokenUploadRequest.builder()
-                .deviceToken("notificationDeviceToken")
-                .clientType(ClientType.WEB)
-                .build();
-
-        String reqBody = objectMapper.writeValueAsString(request);
-
-        request.setMemberId(memberId);
-        NotificationTokenDto created = NotificationTokenDto.builder()
-                .notificationTokenId(1L)
-                .memberId(memberId)
-                .deviceToken(request.getDeviceToken())
-                .clientType(request.getClientType())
-                .build();
-
-        when(SecurityUtils.getCurrentMemberId()).thenReturn(Optional.of(memberId));
-        when(notificationTokenManageService.save(request)).thenReturn(created);
-
-        // Act
-        ResultActions actions = mockMvc.perform(post("/api/notifications/tokens")
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .header(HttpHeaders.AUTHORIZATION, "Bearer token")
-                .content(reqBody)
-        );
-
-        // Assert
-        actions.andExpect(status().isCreated())
-                .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(jsonPath("notificationTokenId").value(created.getNotificationTokenId()))
-                .andDo(print());
-    }
-
-    @Test
-    void saveNotificationToken_400_BadRequest_TokenIsNull() throws Exception {
-        // Arrange
-        NotificationTokenUploadRequest request = NotificationTokenUploadRequest.builder()
-                .clientType(ClientType.WEB)
-                .build();
-
-        // Act
-        ResultActions actions = mockMvc.perform(post("/api/notifications/tokens")
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .header(HttpHeaders.AUTHORIZATION, "Bearer token")
-                .content(objectMapper.writeValueAsString(request))
-        );
-
-        // Assert
-        actions.andExpect(status().isBadRequest())
-                .andDo(print());
-    }
-
-    @Test
-    void saveNotificationToken_401_Unauthorized() throws Exception {
-        // Arrange
-        Long memberId = 1L;
-        NotificationTokenUploadRequest request = NotificationTokenUploadRequest.builder()
-                .deviceToken("notificationDeviceToken")
-                .clientType(ClientType.WEB)
-                .build();
-
-        when(SecurityUtils.getCurrentMemberId()).thenReturn(Optional.empty());
-
-        // Act
-        ResultActions actions = mockMvc.perform(post("/api/notifications/tokens")
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .header(HttpHeaders.AUTHORIZATION, "Bearer token")
-                .content(objectMapper.writeValueAsString(request))
-        );
-
-        // Assert
-        actions.andExpect(status().isUnauthorized())
-                .andDo(print());
-    }
-
-    @Test
-    void removeNotificationToken() throws Exception {
-        // Arrange
-        Long memberId = 1L;
-        Long notificationTokenId = 12314112897L;
-
-        when(SecurityUtils.getCurrentMemberId()).thenReturn(Optional.of(memberId));
-        doNothing().when(notificationTokenManageService).remove(memberId, notificationTokenId);
-
-        // Act
-        ResultActions actions = mockMvc.perform(delete("/api/notifications/tokens/{notificationTokenId}", notificationTokenId)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .header(HttpHeaders.AUTHORIZATION, "Bearer token")
-        );
-
-        // Assert
-        actions.andExpect(status().isNoContent())
                 .andDo(print());
     }
 
